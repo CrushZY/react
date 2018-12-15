15b6567f6d95d46987aa2f0bd3edb89db12941fa
zhangyu <1554398491@qq.com>
2018-12-15 19:04:40 +0800

    React PhotoWall

---
+++ data/imageDatas.js
@@ -0,0 +1,82 @@
+var imgDatas = [
+  {
+    "fileName": "1.jpg",
+    "title": "暹罗猫",
+    "desc": "暹罗猫是世界著名的短毛猫，也是短毛猫的代表品种."
+  },
+  {
+    "fileName": "2.jpg",
+    "title": "布偶猫",
+    "desc": "是现存体型最大、体重最重的猫之一."
+  },
+  {
+    "fileName": "3.jpg",
+    "title": "折耳猫",
+    "desc": "是猫的一种在耳朵有基因突变的猫种."
+  },
+  {
+    "fileName": "4.jpg",
+    "title": "短毛猫",
+    "desc": "大而圆的眼睛根据被毛不同而呈现各种颜色. "
+  },
+  {
+    "fileName": "5.jpg",
+    "title": "波斯猫",
+    "desc": "波斯猫是最常见的长毛猫. "
+  },
+  {
+    "fileName": "6.jpg",
+    "title": "蓝猫",
+    "desc": "历史上曾被称做阿契安吉蓝猫. "
+  },
+  {
+    "fileName": "7.jpg",
+    "title": "短毛猫",
+    "desc": "是家猫中的传统品种. "
+  },
+  {
+    "fileName": "8.jpg",
+    "title": "异国短毛猫",
+    "desc": "美国短毛猫和波斯猫进行交配繁育. "
+  },
+  {
+    "fileName": "9.jpg",
+    "title": "挪威森林猫",
+    "desc": "这是斯堪地半岛特有的品种. "
+  },
+  {
+    "fileName": "10.jpg",
+    "title": "孟买猫",
+    "desc": "孟买猫又称小黑豹. "
+  },
+  {
+    "fileName": "11.jpg",
+    "title": "缅因猫",
+    "desc": "是北美自然产生的第一个长毛猫品种. "
+  },
+  {
+    "fileName": "12.jpg",
+    "title": "埃及猫",
+    "desc": "埃及猫属于中型短毛猫品种.  "
+  },
+  {
+    "fileName": "13.jpg",
+    "title": "伯曼猫",
+    "desc": "伯曼猫又称缅甸圣猫.  "
+  },
+  {
+    "fileName": "14.jpg",
+    "title": "加拿大无毛猫",
+    "desc": "加拿大无毛猫亦称斯芬克斯猫.  "
+  },
+  {
+    "fileName": "15.jpg",
+    "title": "缅甸猫",
+    "desc": "以圆著称.  "
+  },
+  {
+    "fileName": "16.jpg",
+    "title": "阿比西尼亚猫",
+    "desc": "是古埃及被崇拜为“神圣之物”的古埃及猫的后裔.  "
+  }
+]
